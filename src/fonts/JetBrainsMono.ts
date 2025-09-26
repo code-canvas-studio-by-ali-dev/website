9468eac7951fc79b41ea4c64ab9fdc20a3c78ec2
Ali Sulman <alithedeveloper1470@gmail.com>
2025-09-26 21:50:15 +0500

    Landing Page

---
+++ src/fonts/JetBrainsMono.ts
@@ -0,0 +1,8 @@
+import { JetBrains_Mono } from "next/font/google";
+
+const jetBrainsMono = JetBrains_Mono({
+    variable: "--font-jetbrains-mono",
+    subsets: ["latin"],
+});
+
+export default jetBrainsMono;
